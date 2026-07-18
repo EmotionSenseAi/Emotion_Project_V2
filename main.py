9fe260e261332c6897e0bffea8c4779969ca0a6a
Project <adelekeseun725@gmail.com>
2026-07-18 11:23:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ import gdown
 
 if not os.path.exists(Path(__file__).resolve().parent / "new_trained_model.pth"):
     print("Downloading model from Google Drive...")
-    url= "https://drive.google.com/uc?export=download&id=1DyCpytFnYqvurobyDhL4C6RnuQJRwlnU"
-    gdown.download(url, str(Path(__file__).resolve().parent / "new_trained_model.pth"), quiet=False)
+    url= "https://docs.google.com/uc?export=download&confirm=t&id=1DyCpytFnYqvurobyDhL4C6RnuQJRwlnU"
+    gdown.download(url, str(Path(__file__).resolve().parent / "new_trained_model.pth"), quiet=False, fuzzy=True)
     print("Download complete!")
                    
 
